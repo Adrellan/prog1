16048b2afd40afd7298656b70e926fdf08503e99
Adrellan <2001adrian.fekete@gmail.com>
2022-02-23 22:09:38 +0100

    drill12 update

--- week01/drill12.cpp
+++ week01/drill12.cpp
@@ -25,7 +25,7 @@ int main(){
         win.set_label("Axis");
         win.wait_for_button();
 
-        Axis ya {Axis::y, Point {20, 200}, 200, 10, "y axis"};
+        Axis ya {Axis::y, Point {20, 300}, 280, 10, "y axis"};
         ya.set_color(Color::cyan); //cyan színű lesz az y tengely
         win.attach(ya);
         win.set_label("y axis");
@@ -35,6 +35,7 @@ int main(){
         win.attach(sine);
         win.set_label("Sine function");
         win.wait_for_button();
+        sine.set_color(Color::blue);
 
         Polygon poly; //ábrázolni fogunk, (háromszöget most?)
         poly.add(Point{300,200});
@@ -50,13 +51,63 @@ int main(){
         Rectangle r {Point{200, 200}, 100, 50}; //négyszöget rajzolunk
         win.attach(r);
 
-        Closed_polyline poly_rect; //másik megoldás a négyszögre
+        Closed_polyline poly_rect; //ötszög
         poly_rect.add(Point{100, 50});
         poly_rect.add(Point{200, 50});
         poly_rect.add(Point{200, 100});
+        poly_rect.add(Point{100, 100});
         poly_rect.add(Point{50, 75});
 
         win.set_label("Draw rectangle");
+        win.attach(poly_rect);
+        win.wait_for_button();
+
+        r.set_fill_color(Color::yellow);
+        poly.set_style(Line_style(Line_style::dash,4));
+        poly_rect.set_style(Line_style(Line_style(Line_style::dash,2)));
+        poly_rect.set_fill_color(Color::green);
+        win.set_label("Canvas#7");
+        win.wait_for_button();
+
+        Text t {Point{150,150}, "Hello, graphival world!!"};
+        win.attach(t);
+        win.set_label("Canvas #8");
+        win.wait_for_button();
+
+        t.set_font(Graph_lib::Font::times_bold);
+        t.set_font_size(20);
+        win.set_label("Canvas #9");
+        win.wait_for_button();
+
+        Image ii {Point{100,50}, "image.jpg"};
+        win.attach(ii);
+        win.set_label("Canvas #10");
+        win.wait_for_button();
+
+        ii.move(100,200);
+        win.set_label("Canvas #11");
+        win.wait_for_button();
+
+        Circle c {Point{100,200},50};
+        Ellipse e {Point{100,200}, 75,25};
+        e.set_color(Color::dark_red);
+        Mark m {Point{100,200},'x'};
+
+        ostringstream oss;
+        oss << "Screen size: " << x_max() << "*" << y_max()
+            << "; window size: " << win.x_max() << win.y_max();
+        Text sizes {Point{100,200},oss.str()};
+
+        Image cal {Point{225,225}, "snow.gif"};
+        cal.set_mask(Point{40,40},200,150);
+
+        win.attach(c);
+        win.attach(m);
+        win.attach(e);
+
+        win.attach(sizes);
+        win.attach(cal);
+        win.set_label("Cavas #12");
         win.wait_for_button();
 
     } catch (exception& e) {
