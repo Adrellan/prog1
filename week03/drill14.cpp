2d59c77eb9469b944a48d7ebebf803e8926049c9
Adrellan <2001adrian.fekete@gmail.com>
2022-03-01 15:16:45 +0100

    drill14

--- week03/drill14.cpp
+++ week03/drill14.cpp
@@ -7,12 +7,13 @@ class B1{
     public:
         virtual void vf() {cout<<"B1::vf()"<<endl;}     //virtuallal jelezzük h ez felülírható
         void f() {cout<<"B1::f()"<<endl;}
-        virtual void pvf() = 0;                         //nincsen implementációja/működése ~ tisztán virtuális/abstract -> nem példányosítható
+        //virtual void pvf() = 0;                         //nincsen implementációja/működése ~ tisztán virtuális/abstract -> nem példányosítható
 };
 
 class D1: public B1{                                    //lehet szabályozni a mi a legmagasabb öröklődési szint
     public:
         void vf() {cout<<"D1::vf()"<<endl;}
+        void f() {cout<<"D1::f()"<<endl;}
 };
 
 class D2: public D1{
@@ -43,7 +44,7 @@ void f(B2& b2){
 }
 
 int main(){
-    /*B1 b1;
+    B1 b1;
     b1.vf();
     b1.f();
 
@@ -53,7 +54,7 @@ int main(){
 
     B1& b1r = d1;       //referenciát hozok létre
     b1r.vf();
-    b1r.f();*/
+    b1r.f();
 
     //4est otthon
 
